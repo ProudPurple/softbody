64e1327b20406da2162a67590dbebf450691c5ff
ProudPurple <andersonkimball5@gmail.com>
2025-12-06 19:29:22 -0500

    updating and creating made woohoo baby

--- src/softbody.cpp
+++ src/softbody.cpp
@@ -1,13 +1,18 @@
 #include "softbody.h"
 #include <godot_cpp/core/class_db.hpp>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
 
 using namespace godot;
+using namespace std;
 
 void SoftBody2D::_bind_methods() {
 }
 
 SoftBody2D::SoftBody2D() {
 	// Initialize any variables here.
+	set_process(true);
 	time_passed = 0.0;
 }
 
@@ -15,10 +20,31 @@ SoftBody2D::~SoftBody2D() {
 	// Add your cleanup here.
 }
 
+void SoftBody2D::_ready() {
+	cur_poly = get_polygon();
+	define_new_polygon();
+}
+
 void SoftBody2D::_process(double delta) {
 	time_passed += delta;
+	update_polygon();
+}
 
-	Vector2 new_position = Vector2(15 * (10.0 + (10.0 * sin(time_passed * 2.0))),(10.0 + (10.0 * cos(time_passed * 1.5))));
-	set_rotation(get_rotation() + 0.05);
-	set_position(new_position);
+void SoftBody2D::update_polygon() {
+	PackedVector2Array new_poly;
+	for (int i = 0; i < target_poly.size(); i++) {
+		new_poly.push_back(cur_poly[i].lerp(target_poly[i], 1));
+		target_poly[i] += Vector2(-5,5);
+	}
+	cur_poly = new_poly;
+	set_polygon(new_poly);
+}
+
+void SoftBody2D::define_new_polygon() {
+	target_poly.clear();
+	for (int i = 0; i < get_polygon().size() + 3; i++) {
+		if (cur_poly.size() <= i)
+			cur_poly.push_back(cur_poly[cur_poly.size() - 1] + Vector2(rand() % 5 * 10, rand() % 5 * -10));
+		target_poly.push_back(cur_poly[i] + Vector2(100, -100));
+	}
 }
--- src/softbody.h
+++ src/softbody.h
@@ -1,5 +1,7 @@
 #pragma once
-
+#include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/classes/tween.hpp>
+#include <godot_cpp/classes/method_tweener.hpp>
 #include <godot_cpp/classes/polygon2d.hpp>
 #include <godot_cpp/classes/collision_polygon2d.hpp>
 
@@ -10,6 +12,8 @@ class SoftBody2D : public CollisionPolygon2D{
 
 private:
 	double time_passed;
+	PackedVector2Array target_poly;
+	PackedVector2Array cur_poly;
 
 protected:
 	static void _bind_methods();
@@ -18,7 +22,10 @@ public:
 	SoftBody2D();
 	~SoftBody2D();
 
+	void _ready();
 	void _process(double delta) override;
+	void update_polygon();
+	void define_new_polygon();
 };
 
 } // namespace godot
